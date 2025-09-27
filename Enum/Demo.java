73b35e31d6266627bb5a20c07cb3072ee3050676
Ankit Saraswat <ankit1256saraswat@gmail.com>
2025-09-27 21:57:43 +0530

    Interface

--- Enum/Demo.java
+++ Enum/Demo.java
@@ -1,17 +1,29 @@
 
 
-enum Status{
-    Rejected, Pending, Completed,Waiting
+enum Car{
+    Forturner(50), Creta(25), Discovery(125), Fronxx(8);
+      private int price;
+    private Car(int price){
+        this.price=price;
+    }
+    public int getPrice(){
+        return price;
+    }
 }
 
 
 public class Demo {
     public static void main(String[] args) {
-        Status ss[]=Status.values();
+        Car ss[]=Car.values();
         
 
-        for(Status s: ss){
-            System.out.println(s+" "+s.ordinal());
+        for(Car s: ss){
+            System.out.println(s+" "+s.ordinal());//   Ordinal is used for finding the index of the enum just like as Array's index which starts from 0; 
         }
+
+
+        Car c=Car.Creta;
+
+        System.out.println(c+" "+c.getPrice());
     }
 }
---
+++ OOPs/Interface/Demo.java
@@ -0,0 +1,42 @@
+package OOPs.Interface;
+
+/*@FunctionalInterface
+interface A{
+    void show();
+}
+
+
+
+public class Demo {
+    public static void main(String[] args) {
+      A obj=new A() {
+        public void show(){
+            System.out.println("i am implemention of Interface of A");
+        }
+      };
+      obj.show();
+    }
+}*/
+
+
+// Now i will use Lambda Expression in Functional interface
+//Oracle introduced Lambda expression in Java 8
+//We can use Lambda expression in functinal Interface because in functional interface it has only one method,
+// so instead of writting full name , like public void show(), we just use the Lambda interface
+
+
+
+
+interface  A{
+    
+    void show(int num);
+}
+
+class Demo{
+    public static void main(String[] args) {
+         A obj=(int num)-> {
+            System.out.println("this is implementation of Lambda expression in Interface: "+num);
+         };
+         obj.show(5);
+    }
+}
