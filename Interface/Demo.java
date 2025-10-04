5bfc7f1fdd4a0db1b1b41dd1175cdaffdfc7122d
Ankit Saraswat <ankit1256saraswat@gmail.com>
2025-10-04 22:04:58 +0530

    threads

--- Interface/Demo.java
+++ Interface/Demo.java
@@ -4,5 +4,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hii");
         System.out.println("hello");
+        String s1="ankit";
+        String s2="sharma";
+        System.out.println(s1==s2);
     }
 }
