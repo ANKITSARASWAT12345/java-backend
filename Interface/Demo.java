bb660c2399dc2260954f4ac37beae63591ea5ea9
Ankit Saraswat <ankit1256saraswat@gmail.com>
2025-10-12 22:19:29 +0530

    Interface

--- Interface/Demo.java
+++ Interface/Demo.java
@@ -19,7 +19,7 @@ public class Demo {
 */
 
 
-class Sde{
+/*class Sde{
     public void job(){
         System.out.println("your job is to write algorithms");
     }
@@ -35,5 +35,49 @@ class Demo{
     public static void main(String[] args) {
         Sde obj=new Sde();
         obj.job();
+    }
+}*/
+
+
+
+//class - class --> extends
+//class - interface-->implements
+//interface- interface---> extends
+
+
+ interface A extends X {
+
+    int a=9;  //Methods in Interface are by default Final and Static
+   
+    public abstract void show();
+   void config();//methods in Interface are bydefault public abstract
+     
+
+}
+
+interface X{
+    void eat();
+}
+
+class B implements A{
+     public void show(){
+        System.out.println("in show method");
+     }
+
+     public void config(){
+        System.out.println("in config method");
+     }
+     public void eat(){
+        System.out.println("eating... ");
+     }
+}
+
+class Demo{
+    public static void main(String[] args) {
+        
+        A obj=new B();
+        obj.show();
+        obj.config();
+        obj.eat();
     }
 }
