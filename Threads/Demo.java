0b3520bf2ef9f06b5e199f0414a21d179ad1af94
Ankit Saraswat <ankit1256saraswat@gmail.com>
2025-10-29 22:52:47 +0530

    Threads & Mutation

--- Threads/Demo.java
+++ Threads/Demo.java
@@ -42,7 +42,7 @@ public class Demo {
 //Now we will implements Runnable class instead of extending Thread class , because Runnable is parent class of Thread 
 
 
-class Z{
+/*class Z{
     public void m1(){
         System.out.println("hello");
     }
@@ -78,5 +78,47 @@ class Demo{
         B obj3=new B();
         obj1.start();
         obj3.start();
+    }
+}*/
+
+
+
+
+//Thread +Mutation
+
+
+
+class Counter{
+   int count;
+   public synchronized void   increment(){
+    count++;
+   }
+}
+
+public class Demo{
+    public static void main(String[] args) throws InterruptedException {
+        
+
+        Counter c=new Counter();
+        Runnable obj1=()->
+        {
+           for(int i=0;i<10000;i++){
+               c.increment();
+           }
+        };
+        Runnable obj2=()->{
+            for(int i=0;i<10000;i++){
+                c.increment();
+            }
+        };
+
+        Thread t1=new Thread(obj1);
+        Thread t2=new Thread(obj2);
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("count will be: "+c.count);
     }
 }
